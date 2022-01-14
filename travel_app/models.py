cf1c5280ea7f4b28c31b619dedf8d8d53ebd2052
nealgarcia <nathnlgarcia@gmail.com>
2022-01-14 13:26:54 -0600

    created model for trip entry

--- travel_app/models.py
+++ travel_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class TripEntry(models.Model):
+    location = models.CharField(max_length = 50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
